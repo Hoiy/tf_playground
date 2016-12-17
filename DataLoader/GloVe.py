b61f97922223a4e26cc27c44633f536f42fb9938
Hoiy <hoiy927@gmail.com>
2016-12-17 12:26:04 +0800

    Add dataloader.

---
+++ DataLoader/GloVe.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import pandas as pd
+
+def load(file):
+    print("Start: Loading Glove Model")
+    data = pd.read_csv(
+        file,
+        header=None,
+        index_col=0,
+        delim_whitespace=True,
+        quoting=3
+    )
+    print("End: Loaded %d rows." % data.shape[0])
+    return data
+
+def test():
+    glove = load('/Users/Shared/data/glove.6B/glove.6B.50d.txt')
+    print(glove.loc['hello'])
+
+if __name__=='__main__':
+    test()
