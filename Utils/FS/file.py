10fc5470bd827f2081db416cf1bf3ce7004b9d0f
Hoiy <hoiy927@gmail.com>
2017-04-15 14:23:20 +0800

    Backup

--- Utils/FS/file.py
+++ Utils/FS/file.py
@@ -14,16 +14,38 @@ def write(path, data):
 
 def pkl_read(path):
     with open(path, 'rb') as f:
-            return pickle.load(f)
+        return pickle.load(f)
 
 def pkl_write(path, data):
     with open(path, 'wb') as f:
-            return pickle.dump(data, f)
+        return pickle.dump(data, f)
+
+def txt_read(path):
+    try:
+        with open(path, 'r') as f:
+            return f.read()
+    except:
+        print('Failed to load ' + path)
+        return ""
+
+def txt_write(path, data):
+    with open(path, 'w') as f:
+        f.write(data)
+
+def ls(path):
+    path = os.path.abspath(path)
+    return [os.path.join(path, f) for f in os.listdir(path)]
 
 def test():
    obj = [1, 2]
    write('/tmp/test.pkl', obj)
    print(read('/tmp/test.pkl'))
 
+def test2():
+   files = ls('./data/Gutenberg')
+   corpus = ''
+   for f in files:
+       corpus += read(f)
+
 if __name__=='__main__':
-    test()
+    test2()
