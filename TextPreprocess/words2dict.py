6997f55b7bcd69c76a32d6a7eb100ffbd1861ee1
Hoiy <hoiy927@gmail.com>
2017-04-30 10:07:20 +0000

    Backup.

---
+++ TextPreprocess/words2dict.py
@@ -0,0 +1,17 @@
+from collections import Counter
+import operator
+
+# sorted by desc frequency as nce_loss function use uni-log sampling
+def convert(words):
+    counter = Counter(words)
+    word_dict = {}
+    index = 0
+    for item in sorted(counter.items(), key=operator.itemgetter(1), reverse=True):
+        word_dict[item[0]] = index
+        index += 1
+     
+    return word_dict, { v:k for k, v in word_dict.items()}
+
+
+if __name__=='__main__':
+    pass
