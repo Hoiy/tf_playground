338498ba71b916b1e920e34e9493465044a3b0c4
Hoiy <hoiy927@gmail.com>
2017-05-20 05:29:39 +0000

    Backup.

---
+++ Corpus/brown.py
@@ -0,0 +1,14 @@
+from nltk.corpus import brown
+
+def corpus(toLower=True, verbose=0):
+    sents = brown.sents()
+    words = brown.words()
+    if verbose:
+        print("Number of tokens: {}".format(len(words)))
+        print("Number of sentences: {}".format(len(sents)))
+        print("Longest sentences length: {}".format(max([len(sent) for sent in sents])))
+    return sents, words
+
+if __name__=='__main__':
+    pass
+
