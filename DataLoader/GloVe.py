a88910573c584e038e7dbe136a416b8ec0c6ac07
Hoiy <hoiy927@gmail.com>
2017-06-20 16:50:33 +0000

    Backup.

--- DataLoader/GloVe.py
+++ DataLoader/GloVe.py
@@ -14,22 +14,25 @@ def load(file):
     print("End: Loaded %d rows." % data.shape[0])
     return data
 
-def load2(file = '/Users/Shared/data/glove.6B/glove.6B.50d.txt'):
+def load2(file = '/Users/Shared/data/glove.6B/glove.6B.50d.txt', dim=50):
     print("Start: Loading Glove Model")
     f = open(file,'r', encoding='utf8')
     model = {}
     for line in f:
         splitLine = line.split()
         word = splitLine[0]
-        embedding = [float(val) for val in splitLine[1:]]
-        model[word] = embedding
+        try:
+            embedding = [float(val) for val in splitLine[1:dim+1]]
+            model[word] = embedding
+        except:
+            pass
+
     print("End: Loaded %d rows." % len(model))
     return model
 
 def test():
-    glove = load('/Users/Shared/data/glove.6B/glove.6B.50d.txt')
-    print(glove)
-    print(glove.loc['the'])
+    import os
+    glove = load2(os.getcwd() + '/data/GloVe/glove.840B.300d.txt')
 
 if __name__=='__main__':
     test()
